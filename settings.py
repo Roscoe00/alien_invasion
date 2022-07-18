c9dca1b8001cf74ab838f43bcf3a875e14c9744d
Roscoe Bull <roscoe-bull@live.co.uk>
2022-07-18 17:48:54 +0100

    adding files to gitHub

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all setting for Alien Invasion"""
+
+    def __init__(self):
+        """Initialise the game's settings."""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        # Set the background colour.
+        self.bg_color = (230,230,230)
